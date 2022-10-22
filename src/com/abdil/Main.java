8d95dc9beebb709a1beed6595becfbe7e702e813
achmdabdillah <achmdabdil9@gmail.com>
2022-10-22 14:28:16 +0700

    leet code problem 1

--- src/com/abdil/Main.java
+++ src/com/abdil/Main.java
@@ -2,6 +2,32 @@ package com.abdil;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        char[] arr = {'a','b'};
+        char target = 'z';
+
+        char ans = nextGreatestLetter(arr, target);
+        System.out.println(ans);
+    }
+
+    static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+
+        if(target > letters[letters.length -1]){
+            return letters[start % letters.length];
+        }
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(target < letters[mid]){
+                    end = mid - 1;
+            }
+            else if(target > letters[mid]){
+                    start = mid + 1;
+            }
+        }
+
+        return letters[start];
     }
 }
--- src/com/abdil/SmallestLetter.java
+++ src/com/abdil/SmallestLetter.java
@@ -1,1 +1,26 @@
+package com.abdil;
 
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+public class SmallestLetter {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+
+        if(target > letters[letters.length -1]){
+            return letters[start % letters.length];
+        }
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(target < letters[mid]){
+                    end = mid - 1;
+            }
+            else if(target > letters[mid]){
+                    start = mid + 1;
+            }
+        }
+
+        return letters[start];
+    }
+}
